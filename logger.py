455b55da970bbb4f84317ef9932a27edfb455a8f
Anthony Courchesne <anthony.courchesne@mail.mcgill.ca>
2020-08-21 14:50:25 -0400

    initial commit

---
+++ logger.py
@@ -0,0 +1,113 @@
+from concurrent.futures import ThreadPoolExecutor
+
+import os
+import pickle
+import numpy as np
+
+from typing import List,Dict
+
+from log_schema import Episode, Step
+
+SCHEMA_VERSION = "1.0.0"
+
+class Logger:
+    def __init__(self, env, log_file):
+        self.env = env
+        self.episode = Episode(version=SCHEMA_VERSION)
+        self.episode_count = 0
+
+        self._log_file = open(log_file, 'wb')
+        # we log the data in a multithreaded fashion
+        self._multithreaded_recording = ThreadPoolExecutor(4)
+        # self.recording = []
+
+    # def log(self, observation, action, reward, done, info):
+    #     x, y, z = self.env.cur_pos
+    #     self.recording.append({
+    #         'step': [
+    #             observation,
+    #             action,
+    #         ],
+    #         # this is metadata, you may not use it at all, but it may be helpful for debugging purposes
+    #         'metadata': [
+    #             (x, y, z, self.env.cur_angle),  # we store the pose, just in case we need it
+    #             reward,
+    #             done,
+    #             info
+    #         ]
+    #     })
+    
+    def log(self, step: Step, info: Dict):
+        if self.episode.metadata is None:
+            self.episode.metadata = info
+        self.episode.steps.append(step)
+
+    def on_episode_done(self):
+        print(f"episode {self.episode_count} done, writing to file")
+        self._multithreaded_recording.submit(lambda: self._commit(self.episode))
+        self.episode = Episode(version=SCHEMA_VERSION)
+        self.episode_count += 1
+
+    def _commit(self, episode):
+        # we use pickle to store our data
+        # pickle.dump(self.recording, self._log_file)
+        pickle.dump(episode, self._log_file)
+        self._log_file.flush()
+        # del self.recording[:]
+        # self.recording.clear()
+
+    def close(self):
+        self._multithreaded_recording.shutdown()
+        self._log_file.close()
+        os.chmod(self._log_file.name, 0o444)  # make file read-only after finishing
+
+class SteeringToWheelVelWrapper:
+    """ Converts policy that was trained with [velocity|heading] actions to
+    [wheelvel_left|wheelvel_right] to comply with AIDO evaluation format
+    """
+
+    def __init__(self, gain=1.0, trim=0.0, radius=0.0318, k=27.0, limit=1.0, wheel_dist=0.102):
+        # Should be adjusted so that the effective speed of the robot is 0.2 m/s
+        self.gain = gain
+
+        # Directional trim adjustment
+        self.trim = trim
+
+        # Wheel radius
+        self.radius = radius
+
+        # Motor constant
+        self.k = k
+
+        # Wheel velocity limit
+        self.limit = limit
+
+        # Distance between wheels
+        self.wheel_dist = wheel_dist
+
+    def convert(self, vel, angle):
+
+        # Distance between the wheels
+        baseline = self.wheel_dist
+
+        # assuming same motor constants k for both motors
+        k_r = self.k
+        k_l = self.k
+
+        # adjusting k by gain and trim
+        k_r_inv = (self.gain + self.trim) / k_r
+        k_l_inv = (self.gain - self.trim) / k_l
+
+        omega_r = (vel + 0.5 * angle * baseline) / self.radius
+        omega_l = (vel - 0.5 * angle * baseline) / self.radius
+
+        # conversion from motor rotation rate to duty cycle
+        u_r = omega_r * k_r_inv
+        u_l = omega_l * k_l_inv
+
+        # limiting output to limit, which is 1.0 for the duckiebot
+        u_r_limited = max(min(u_r, self.limit), -self.limit)
+        u_l_limited = max(min(u_l, self.limit), -self.limit)
+
+        vels = np.array([u_l_limited, u_r_limited])
+        return vels
